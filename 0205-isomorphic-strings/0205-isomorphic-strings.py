78d80cde885acbc5968281b96b43f72cf56ea7f5
jungyu <96876293+jungyu333@users.noreply.github.com>
2026-04-06 19:35:28 +0900

    Time: 3 ms (91.39%), Space: 19.1 MB (89.03%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,25 +1,21 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         
-        def helper(s, t ,hash):
-            for char_s, char_t in zip(s, t):
-            
-                if char_s != char_t:
-                    if char_s not in hash:
-                        hash[char_s] = char_t
-                    elif hash.get(char_s) != char_t:
-                        return False
-                else:
-                    if char_s not in hash:
-                        hash[char_s] = char_t
-                    else:
-                        if hash[char_s] != char_t:
-                            return False
-                        
-            
-            return True
+        s_to_t = {}
+        t_to_s = {}
 
-        result = helper(s,t,{})
-        reverse_result = helper(t,s,{})
+        for char_s, char_t in zip(s, t):
 
-        return result and reverse_result
+            if char_s in s_to_t:
+                if s_to_t.get(char_s) != char_t:
+                    return False
+            else:
+                s_to_t[char_s] = char_t
+
+            if char_t in t_to_s:
+                if t_to_s.get(char_t) != char_s:
+                    return False
+            else:
+                t_to_s[char_t] = char_s
+        
+        return True
