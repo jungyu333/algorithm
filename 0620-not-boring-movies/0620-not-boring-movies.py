a0834ba1f6386beef778d0be15691593a2a38f9d
jungyu <96876293+jungyu333@users.noreply.github.com>
2026-04-22 21:38:41 +0900

    Time: 264 ms (61.42%), Space: 67.1 MB (74.15%) - LeetHub

---
+++ 0620-not-boring-movies/0620-not-boring-movies.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def not_boring_movies(cinema: pd.DataFrame) -> pd.DataFrame:
+    
+    cinema = cinema[(cinema['id'] % 2 == 1) & (cinema['description'] != 'boring')]
+    
+    return cinema.sort_values(by='rating', ascending=False)
