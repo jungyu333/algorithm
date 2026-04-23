37596ead3f567e0e40ff3a343423d4a0dbc87de2
jungyu <96876293+jungyu333@users.noreply.github.com>
2026-04-23 19:39:38 +0900

    Time: 0 ms (100%), Space: 19.3 MB (49.36%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -3,15 +3,8 @@ from collections import Counter
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
-        num1_set = list(set(nums1))
-        num2_set = list(set(nums2))
+        num1_set = set(nums1)
+        num2_set = set(nums2)
 
-        num1_counters = Counter(num1_set)
-
-        result = []
-
-        for num in num2_set:
-            if num1_counters.get(num):
-                result.append(num)
-        
+        result = list(num1_set & num2_set)
         return result
