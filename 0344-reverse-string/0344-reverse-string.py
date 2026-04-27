e63b19adced83fdd721a40d0690ac489978f2bb6
jungyu <96876293+jungyu333@users.noreply.github.com>
2026-04-27 19:53:29 +0900

    Time: 7 ms (11.15%), Space: 23.4 MB (47.7%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -9,10 +9,7 @@ class Solution:
 
         while start < end:
 
-            temp = s[start]
-
-            s[start] = s[end]
-            s[end] = temp
+            s[start], s[end] = s[end], s[start]
 
             start += 1
             end -= 1
