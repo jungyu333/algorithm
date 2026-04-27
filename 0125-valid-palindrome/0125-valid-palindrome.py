7569c1c6f612877de6c6653cd50dc7f494262e56
jungyu <96876293+jungyu333@users.noreply.github.com>
2026-04-27 20:02:10 +0900

    Time: 3 ms (98.97%), Space: 20.8 MB (20.23%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,20 +2,9 @@ import re
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        
-        transform = ''
 
         s = re.sub(r'[^a-z0-9]', '', s.lower())
 
-        start = 0
-        end = len(s) - 1
-
-        while start < end :
-            
-            if s[start] != s[end]:
-                return False
-            
-            start += 1
-            end -= 1
+        s_reverse = ''.join(reversed(s))
         
-        return True
+        return s == s_reverse
